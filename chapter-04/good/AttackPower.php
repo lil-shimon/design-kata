f9dc34c5255982f11cccb7babba916bef6a3c8a5
lil-shimon <shimon101018@gmail.com>
2026-09-06 23:02:05 +0900

    update

--- chapter-04/good/AttackPower.php
+++ chapter-04/good/AttackPower.php
@@ -6,8 +6,16 @@ final class AttackPower
 
     public function __construct(private readonly int $value)
     {
-        if ($value < MIN) {
+        if ($value < self::MIN) {
             throw new InvalidArgumentException('attackPowerは'.self::MIN.'以上にしてください。');
         }
+    }
+
+    public function enhance(int $increment): self
+    {
+        if ($increment < 1) {
+            throw new InvalidArgumentException('incrementは1以上にしてください。');
+        }
+        return new self($this->value + $increment);
     }
 }
