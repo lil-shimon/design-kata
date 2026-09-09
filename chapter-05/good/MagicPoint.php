f3c39c9b48a40724cf4f55b0ef4f3a8f5f6b961d
lil-shimon <shimon101018@gmail.com>
2026-09-09 20:54:52 +0900

    update

--- chapter-05/good/MagicPoint.php
+++ chapter-05/good/MagicPoint.php
@@ -2,8 +2,14 @@
 
 class MagicPoint
 {
-    public function __construct(private readonly int $currentMagicPoint, private readonly int $originalMagicPoint, private readonly array $maxMagicPointIncrements) 
-    {}
+    /**
+     * @param list<int> $maxMagicPointIncrements 最大MPの増加量(装備やレベルアップによる)
+     */
+    public function __construct(
+        private readonly int $currentMagicPoint,
+        private readonly int $originalMagicPoint,
+        private readonly array $maxMagicPointIncrements,
+    ) {}
 
     public function current(): int
     {
@@ -19,13 +25,21 @@ class MagicPoint
         return $amount;
     }
 
-    public function recover(int $amount): void
+    public function recover(int $amount): self
     {
-        $this->currentMagicPoint = min($this->max(), $this->currentMagicPoint + $amount);
+        return new self(
+            min($this->max(), $this->currentMagicPoint + $amount),
+            $this->originalMagicPoint,
+            $this->maxMagicPointIncrements,
+        );
     }
 
-    public function consume(int $amount): void
+    public function consume(int $amount): self
     {
-        $this->currentMagicPoint = max($this->currentMagicPoint - $amount, 0);
+        return new self(
+            max($this->currentMagicPoint - $amount, 0),
+            $this->originalMagicPoint,
+            $this->maxMagicPointIncrements,
+        );
     }
 }
