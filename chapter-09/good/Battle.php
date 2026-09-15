a2f4eb7211b4cab82b961a4389fd47c2ae8769be
lil-shimon <shimon101018@gmail.com>
2026-09-15 23:57:59 +0900

    update

--- chapter-09/good/Battle.php
+++ chapter-09/good/Battle.php
@@ -61,10 +61,11 @@ class Party
 
             $member->hitPoint -= 10;
 
-            if (0 < $member->hitPoint) continue;
-            $member->hitPoint = 0;
-            $member->addState(StateType::Dead);
-            $member->removeState(StateType::Poison);
+            if ($member->hitPoint <= 0) {
+                $member->hitPoint = 0;
+                $member->addState(StateType::Dead);
+                $member->removeState(StateType::Poison);
+            }
         }
     }
 }
