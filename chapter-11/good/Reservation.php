44c72b66ea01692d88640e94e67f1ec9d712440c
lil-shimon <shimon101018@gmail.com>
2026-09-19 16:53:18 +0900

    update

---
+++ chapter-11/good/Reservation.php
@@ -0,0 +1,34 @@
+<?php
+
+class Reservation
+{
+    const DEPOSIT_RATE = 0.1;
+    const MIN_QUANTITY = 1;
+
+    public readonly int $productId;
+    public readonly int $deposit;
+    public readonly int $productPrice;
+
+    public function __construct(
+        public readonly int $userId,
+        Product $product,
+        public readonly DateTimeImmutable $reservedAt,
+        public readonly DateTimeImmutable $releaseAt,
+        public readonly int $quantity
+    )
+    {
+        if ($quantity < self::MIN_QUANTITY) {
+            throw new InvalidArgumentException();
+        }
+
+        $this->productId = $product->id;
+        $this->productPrice = $product->price;
+
+        $this->deposit = (int)($product->price * $quantity * self::DEPOSIT_RATE);
+    }
+
+    public function remainingPayment(): int
+    {
+        return $this->productPrice * $this->quantity - $this->deposit;
+    }
+}
